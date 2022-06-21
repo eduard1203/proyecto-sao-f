4411ae2e5638e52830614d6b9c9be8d6df4b40a8
eduard1203 <eduardacostaleonardo2@gmail.com>
2022-06-21 01:36:03 -0500

    PRUBEA 8

--- src/main/java/com/example/demo/controller/index.java
+++ src/main/java/com/example/demo/controller/index.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping
 public class index {
-	 @GetMapping("/")
+	 @GetMapping("/index")
 	  public  String mostrar() {
 
 		return  "index";
