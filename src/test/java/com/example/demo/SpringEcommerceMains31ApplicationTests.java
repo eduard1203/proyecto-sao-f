3b7f882b58256dc9d941fb8dd90ec43262e10311
eduard1203 <eduardacostaleonardo2@gmail.com>
2022-06-21 16:57:38 -0500

    caegA

--- src/test/java/com/example/demo/SpringEcommerceMains31ApplicationTests.java
+++ src/test/java/com/example/demo/SpringEcommerceMains31ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringEcommerceMains3ApplicationTests {
+class SpringEcommerceMains31ApplicationTests {
 
 	@Test
 	void contextLoads() {
