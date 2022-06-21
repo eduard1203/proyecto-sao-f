3d3bf216f5d6c23b48e3648c2c2f51f41df3c169
eduard1203 <eduardacostaleonardo2@gmail.com>
2022-06-21 09:22:52 -0500

    Update index.java

--- src/main/java/com/example/demo/index.java
+++ src/main/java/com/example/demo/index.java
@@ -1,15 +1,17 @@
-package com.example.demo.controller;
+package com.example.demo;
 
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping
 public class index {
-	
-	
-	@GetMapping("/")
-public String indeX() {
+	 @GetMapping("/")
+	  public  String mostrar() {
 
-	return"index";
+		return  "index";
+	  }
+		
 }
-}
-
-
