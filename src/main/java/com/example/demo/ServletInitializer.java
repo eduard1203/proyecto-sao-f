2e2ef92af2d13fdec6eb9395e60bac7d92868d83
eduard1203 <eduardacostaleonardo2@gmail.com>
2022-06-21 16:44:05 -0500

    carga

--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringEcommerceMainsApplication.class);
+		return application.sources(SpringEcommerceMains3Application.class);
 	}
 
 }
--- src/main/java/com/example/demo/SpringEcommerceMains3Application.java
+++ src/main/java/com/example/demo/SpringEcommerceMains3Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringEcommerceMainsApplication {
+public class SpringEcommerceMains3Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringEcommerceMainsApplication.class, args);
+		SpringApplication.run(SpringEcommerceMains3Application.class, args);
 	}
 
 }
