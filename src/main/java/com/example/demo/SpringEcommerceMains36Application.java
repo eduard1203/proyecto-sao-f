523aac7ed8b29b298021aedf6f4f8bdf8ac04ac5
eduard1203 <eduardacostaleonardo2@gmail.com>
2022-06-22 13:40:20 -0500

    carga

---
+++ src/main/java/com/example/demo/SpringEcommerceMains36Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcommerceMains36Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcommerceMains36Application.class, args);
+	}
+
+}
