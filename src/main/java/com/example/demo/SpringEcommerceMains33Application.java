994d021eb6f72927eb024b19ab9c642c6392a4e3
eduard1203 <eduardacostaleonardo2@gmail.com>
2022-06-22 09:27:44 -0500

    carga

--- src/main/java/com/example/demo/SpringEcommerceMains33Application.java
+++ src/main/java/com/example/demo/SpringEcommerceMains33Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringEcommerceMainsApplication {
+public class SpringEcommerceMains33Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringEcommerceMainsApplication.class, args);
+		SpringApplication.run(SpringEcommerceMains33Application.class, args);
 	}
 
 }
