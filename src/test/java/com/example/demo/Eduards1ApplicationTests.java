aba2330a5466e646b8410a801e508846e41d70df
eduard1203 <eduardacostaleonardo2@gmail.com>
2022-06-23 11:17:24 -0500

    carga
    12

---
+++ src/test/java/com/example/demo/Eduards1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eduards1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
